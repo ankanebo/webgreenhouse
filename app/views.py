b023a6cbbf2f0b8b9c1393bded647d61ffc9a930
user3 <podnebesnovaanna@gmail.com>
2023-02-14 21:13:39 +0300

    работа с формами

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
-# функция представления index опущена для краткости
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -14,6 +14,10 @@ def index():
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', 
         title = 'Sign In',
-        form = form)
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
